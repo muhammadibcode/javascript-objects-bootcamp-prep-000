ced3397a803c40a7e0729bb241021f1ab333e8cd
Muhammad Bhatti <muhammad.i.b@outlook.com>
2019-08-02 19:38:16 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,6 +4,10 @@ function updateObjectWithKeyAndValue(obj, key, value) {
   return Object.assign(obj, { [key]: value })
 }
 
+function updateObjectWithObject(targetObject, updatesObject) {
+  return Object.assign({}, targetObject, updatesObject)
+}
+
 console.log(updateObjectWithKeyAndValue(Prism, "Shape", "Cube"));
 
 console.log(Prism)
