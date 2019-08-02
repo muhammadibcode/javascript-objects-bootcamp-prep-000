5e75fa9b78708225d76d29cd151f331e70ede328
Muhammad Bhatti <muhammad.i.b@outlook.com>
2019-08-02 19:45:29 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,18 +1,3 @@
-var Prism = { Shape: "Sphere" }
+var playlist = { BritneySpears: "Toxic" }
 
-var Flat = { Dimensions: 1 }
-
-function updateObjectWithKeyAndValue(obj, key, value) {
-  return Object.assign(obj, { [key]: value })
-}
-
-function updateObjectWithObject(targetObject, updatesObject) {
-  return Object.assign({}, targetObject, updatesObject)
-}
-
-console.log(updateObjectWithObject(Prism, Flat));
-
-console.log(Prism)
-
-console.log(Flat)
 
